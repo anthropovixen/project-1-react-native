c3acf4abcbe24961c31f5ca181588e8cf87b5bc6
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-12-22 21:52:14 +1100

    Update App.js
    Use styling directly inside the component

--- App.js
+++ App.js
@@ -2,25 +2,22 @@ import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 const App = () => {
-	const [name, setName] = useState('Tanimara');
-	const [session, setSession] = useState({ number: 1, title: 'state' });
-	const [current, setCurrent] = useState(true);
+	const [name, setName] = useState('Styling Test');
 
 	const onClickHandler = () => {
-		setName('Programming with Tanimara');
-		setSession({ number: 2, title: 'changing state' });
-		setCurrent(false);
+		setName('Style Test Done!');
 	};
 
 	return (
-		<View style={styles.body}>
+		<View
+			style={{
+				flex: 1,
+				backgroundColor: 'darkred',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
 			<Text style={styles.text}>{name}</Text>
-			<Text style={styles.text}>
-				This is session number {session.number} and about {session.title}
-			</Text>
-			<Text style={styles.text}>
-				{current ? 'current session' : 'next session'}
-			</Text>
 			<Button title="Update State" onPress={onClickHandler}></Button>
 		</View>
 	);

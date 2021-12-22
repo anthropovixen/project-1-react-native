0e75ecee86bd9a9078e2e51f2a0358a944d21925
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-12-22 21:24:44 +1100

    Update App.js
    Import hook useState
    Create variable to change name on Text
    Create variable to insert session number and title on text
    Create function to show text when pushing button
    Insert name, session and function into state

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Button, Linking } from 'react-native';
 
 const App = () => {
+	const [name, setName] = useState('Tanimara');
+	const [session, setSession] = useState({ number: 6, title: 'state' });
+
+	const onClickHandler = () => {
+		setName('Programming with Tanimara');
+	};
+
 	return (
 		<View style={styles.body}>
-			<Text style={styles.text}>Programming with Tanimara</Text>
-			<Button
-				title="React Native practicing"
-				onPress={() => {
-					Linking.openURL('https://github.com/anthropovixen');
-				}}
-			></Button>
+			<Text style={styles.text}>{name}</Text>
+			<Text style={styles.text}>
+				This is session number {session.number} and about {session.title}
+			</Text>
+			<Button title="Update State" onPress={onClickHandler}></Button>
 		</View>
 	);
 };

943d8e8aad120ddcea4be7b1ce3926dee123f900
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-12-28 21:03:04 +1100

    Update App.js
    Import FlatList to use it on the app
    Remove keys from items
    Return FlatList
    Generate key index automatically
    Transform key index toString
    renderItems inside of View

--- App.js
+++ App.js
@@ -6,29 +6,37 @@ import {
 	ScrollView,
 	RefreshControl,
 } from 'react-native';
+import { FlatList } from 'react-native-gesture-handler';
 
 const App = () => {
 	const [Items, setItems] = useState([
-		{ key: 1, item: 'Item 1' },
-		{ key: 2, item: 'Item 2' },
-		{ key: 3, item: 'Item 3' },
-		{ key: 4, item: 'Item 4' },
-		{ key: 5, item: 'Item 5' },
-		{ key: 6, item: 'Item 6' },
-		{ key: 7, item: 'Item 7' },
-		{ key: 8, item: 'Item 8' },
+		{ name: 'Item 1' },
+		{ name: 'Item 2' },
+		{ name: 'Item 3' },
+		{ name: 'Item 4' },
+		{ name: 'Item 5' },
+		{ name: 'Item 6' },
+		{ name: 'Item 7' },
+		{ name: 'Item 8' },
 	]);
 	const [Refreshing, setRefreshing] = useState(false);
 
 	const onRefresh = () => {
 		setRefreshing(true);
-		setItems([...Items, { key: 69, item: 'Item 69' }]);
+		setItems([...Items, { name: 'Item 69' }]);
 		setRefreshing(false);
 	};
 
 	return (
-		<ScrollView
-			style={styles.body}
+		<FlatList
+		
+			keyExtractor={(item, index) => index.toString()}
+			data={Items}
+			renderItem={({item}) => (
+				<View style={styles.item}>
+		 			<Text style={styles.text}>{item.name}</Text>
+				</View>
+			)}
 			refreshControl={
 				<RefreshControl
 					refreshing={Refreshing}
@@ -36,15 +44,25 @@ const App = () => {
 					colors={['#ff00ff']}
 				/>
 			}
-		>
-			{Items.map((object) => {
-				return (
-					<View style={styles.item} key={object.key}>
-						<Text style={styles.text}>{object.item}</Text>
-					</View>
-				);
-			})}
-		</ScrollView>
+		/>
+		// <ScrollView
+		// 	style={styles.body}
+		// 	refreshControl={
+		// 		<RefreshControl
+		// 			refreshing={Refreshing}
+		// 			onRefresh={onRefresh}
+		// 			colors={['#ff00ff']}
+		// 		/>
+		// 	}
+		// >
+		// 	{Items.map((object) => {
+		// 		return (
+		// 			<View style={styles.item} key={object.key}>
+		// 				<Text style={styles.text}>{object.item}</Text>
+		// 			</View>
+		// 		);
+		// 	})}
+		// </ScrollView>
 	);
 };
 

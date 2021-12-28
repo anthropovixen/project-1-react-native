9750921c8950b6f80ba61530850234e86ac0d324
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-12-28 21:13:49 +1100

    Update App.js
    Import SectionList
    Create array of DATA to display
    Remove FlatList from return
    Return SectionList with a keyExtractor to generate a key index automatically
    Transform key index toString
    Indicate where sections should get data from
    Render items on display
    RenderSectionHeader (titles) on display.

--- App.js
+++ App.js
@@ -5,8 +5,9 @@ import {
 	View,
 	ScrollView,
 	RefreshControl,
+	FlatList,
+	SectionList,
 } from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
 
 const App = () => {
 	const [Items, setItems] = useState([
@@ -19,6 +20,28 @@ const App = () => {
 		{ name: 'Item 7' },
 		{ name: 'Item 8' },
 	]);
+	const DATA = [
+		{
+			title: 'Title 1',
+			data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+		},
+		{
+			title: 'Title 2',
+			data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+		},
+		{
+			title: 'Title 3',
+			data: ['Item 3-1', 'Item 3-2', 'Item 3-3'],
+		},
+		{
+			title: 'Title 4',
+			data: ['Item 4-1', 'Item 4-2', 'Item 4-3'],
+		},
+		{
+			title: 'Title 5',
+			data: ['Item 5-1', 'Item 5-2', 'Item 5-3'],
+		},
+	];
 	const [Refreshing, setRefreshing] = useState(false);
 
 	const onRefresh = () => {
@@ -28,23 +51,33 @@ const App = () => {
 	};
 
 	return (
-		<FlatList
-		
+		<SectionList
 			keyExtractor={(item, index) => index.toString()}
-			data={Items}
-			renderItem={({item}) => (
+			sections={DATA}
+			renderItem={({ item }) => <Text style={styles.text}>{item}</Text>}
+			renderSectionHeader={({ section }) => (
 				<View style={styles.item}>
-		 			<Text style={styles.text}>{item.name}</Text>
+					<Text style={styles.text}>{section.title}</Text>
 				</View>
 			)}
-			refreshControl={
-				<RefreshControl
-					refreshing={Refreshing}
-					onRefresh={onRefresh}
-					colors={['#ff00ff']}
-				/>
-			}
 		/>
+		// <FlatList
+
+		// 	keyExtractor={(item, index) => index.toString()}
+		// 	data={Items}
+		// 	renderItem={({item}) => (
+		// 		<View style={styles.item}>
+		//  			<Text style={styles.text}>{item.name}</Text>
+		// 		</View>
+		// 	)}
+		// 	refreshControl={
+		// 		<RefreshControl
+		// 			refreshing={Refreshing}
+		// 			onRefresh={onRefresh}
+		// 			colors={['#ff00ff']}
+		// 		/>
+		// 	}
+		// />
 		// <ScrollView
 		// 	style={styles.body}
 		// 	refreshControl={

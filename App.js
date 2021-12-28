3b1fd41b8c8971204bd488ab3a815a0843e60c6f
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2021-12-28 21:44:01 +1100

    Update App.js
    Clean environment for exercise with keyboard and text input components
    Import TextInput
    Start with empty State
    Return View with content and Text Input
    Add style to input box
    Add placeholder for inside of input box
    When text is added store it
    If input needs to have more than 1 line
    if keyboard needs to be only numeric
    If input should not be editable
    If input should automatically turn into little dots (as in passwords)
    If input needs to have a maximum Length defined
    Display text with input stored on State
    Create style for input box

--- App.js
+++ App.js
@@ -1,121 +1,46 @@
 import React, { useState } from 'react';
-import {
-	StyleSheet,
-	Text,
-	View,
-	ScrollView,
-	RefreshControl,
-	FlatList,
-	SectionList,
-} from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 const App = () => {
-	const [Items, setItems] = useState([
-		{ name: 'Item 1' },
-		{ name: 'Item 2' },
-		{ name: 'Item 3' },
-		{ name: 'Item 4' },
-		{ name: 'Item 5' },
-		{ name: 'Item 6' },
-		{ name: 'Item 7' },
-		{ name: 'Item 8' },
-	]);
-	const DATA = [
-		{
-			title: 'Title 1',
-			data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
-		},
-		{
-			title: 'Title 2',
-			data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
-		},
-		{
-			title: 'Title 3',
-			data: ['Item 3-1', 'Item 3-2', 'Item 3-3'],
-		},
-		{
-			title: 'Title 4',
-			data: ['Item 4-1', 'Item 4-2', 'Item 4-3'],
-		},
-		{
-			title: 'Title 5',
-			data: ['Item 5-1', 'Item 5-2', 'Item 5-3'],
-		},
-	];
-	const [Refreshing, setRefreshing] = useState(false);
-
-	const onRefresh = () => {
-		setRefreshing(true);
-		setItems([...Items, { name: 'Item 69' }]);
-		setRefreshing(false);
-	};
+	const [name, SetName] = useState('');
 
 	return (
-		<SectionList
-			keyExtractor={(item, index) => index.toString()}
-			sections={DATA}
-			renderItem={({ item }) => <Text style={styles.text}>{item}</Text>}
-			renderSectionHeader={({ section }) => (
-				<View style={styles.item}>
-					<Text style={styles.text}>{section.title}</Text>
-				</View>
-			)}
-		/>
-		// <FlatList
-
-		// 	keyExtractor={(item, index) => index.toString()}
-		// 	data={Items}
-		// 	renderItem={({item}) => (
-		// 		<View style={styles.item}>
-		//  			<Text style={styles.text}>{item.name}</Text>
-		// 		</View>
-		// 	)}
-		// 	refreshControl={
-		// 		<RefreshControl
-		// 			refreshing={Refreshing}
-		// 			onRefresh={onRefresh}
-		// 			colors={['#ff00ff']}
-		// 		/>
-		// 	}
-		// />
-		// <ScrollView
-		// 	style={styles.body}
-		// 	refreshControl={
-		// 		<RefreshControl
-		// 			refreshing={Refreshing}
-		// 			onRefresh={onRefresh}
-		// 			colors={['#ff00ff']}
-		// 		/>
-		// 	}
-		// >
-		// 	{Items.map((object) => {
-		// 		return (
-		// 			<View style={styles.item} key={object.key}>
-		// 				<Text style={styles.text}>{object.item}</Text>
-		// 			</View>
-		// 		);
-		// 	})}
-		// </ScrollView>
+		<View style={styles.body}>
+			<Text style={styles.text}>Please write your name:</Text>
+			<TextInput
+				style={styles.input}
+				placeholder="e.g. Maria"
+				onChangeText={(value) => SetName(value)}
+				// multiline
+				// keyboardType='numeric'
+				// editable={false}
+				// secureTextEntry
+				maxLength={20}
+			/>
+			<Text style={styles.text}>Your name is: {name}</Text>
+		</View>
 	);
 };
 
 const styles = StyleSheet.create({
 	body: {
 		flex: 1,
-		flexDirection: 'column',
 		backgroundColor: '#ffffff',
-	},
-	item: {
-		margin: 10,
-		backgroundColor: 'beige',
-		justifyContent: 'center',
 		alignItems: 'center',
 	},
+
 	text: {
-		color: 'black',
-		fontSize: 48,
-		fontStyle: 'italic',
+		color: '#000044',
+		fontSize: 20,
 		margin: 10,
+	},
+	input: {
+		width: 200,
+		borderWidth: 1,
+		borderColor: 'grey',
+		borderRadius: 5,
+		textAlign: 'center',
+		fontSize: 20,
 	},
 });
 

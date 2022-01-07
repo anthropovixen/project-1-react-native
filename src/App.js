fc86bdb291d579a9b8ef026e7cee2992d1976b80
Tanimara Elias Santos <36615712+anthropovixen@users.noreply.github.com>
2022-01-07 21:49:42 +1100

    Update App.js
    Remove imports unnecessary
    Import NavigationContainer and createStackNavigator
    Create Navigator with two components: ScreenA and ScreenB
    Add navigation as a prop for the two screens
    Define onPressHandler to go to the other screen with navigation commands
    Add onPressHandler to Button
    Create App that displays Navigation Container, Stack Nav with no header in all screens
    Create Stack Screen with name Screen_A and component ScreenA
    Create Stack Screen with name Screen_B and component ScreenB
    Style text

--- src/App.js
+++ src/App.js
@@ -1,176 +1,81 @@
 import React, { useState } from 'react';
-import {
-	StyleSheet,
-	Text,
-	View,
-	TextInput,
-	Pressable,
-	Modal,
-	Image,
-	ImageBackground,
-} from 'react-native';
+import { StyleSheet, Text, View, Pressable } from 'react-native';
 
-import TaniButton from './CustomButton';
-import Header from './Header';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
-	const [name, SetName] = useState('');
-	const [submitted, SetSubmitted] = useState(false);
-	const [showWarning, SetshowWarning] = useState(false);
+const Stack = createStackNavigator();
+
+function ScreenA({ navigation }) {
 	const onPressHandler = () => {
-		if (name.length > 3) {
-			SetSubmitted(!submitted);
-		} else {
-			SetshowWarning(true);
-		}
+		navigation.navigate('Screen_B');
+	};
+	return (
+		<View style={styles.body}>
+			<Text style={styles.text}>Screen A</Text>
+			<Pressable
+				onPress={onPressHandler}
+				style={({ pressed }) => ({
+					backgroundColor: pressed ? '#ddd' : '#d5a6bd',
+				})}
+			>
+				<Text style={styles.text}>Go to Screen B</Text>
+			</Pressable>
+		</View>
+	);
+}
+
+function ScreenB({ navigation }) {
+	const onPressHandler = () => {
+		// navigation.navigate('Screen_A');
+		navigation.goBack();
 	};
 
 	return (
-		<ImageBackground
-			style={styles.body}
-			source={{
-				uri:
-					'https://images.unsplash.com/photo-1547514701-42782101795e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80',
-			}}
-		>
-			<Header />
-			<Modal
-				visible={showWarning}
-				transparent
-				onRequestClose={() => SetshowWarning(false)}
-				animationType="slide"
-				hardwareAccelerated
-			>
-				<View style={styles.centered_view}>
-					<View style={styles.warn_modal}>
-						<View style={styles.warn_title}>
-							<Text style={styles.text}>WARNING!</Text>
-						</View>
-						<View style={styles.warn_body}>
-							<Text style={styles.text}>
-								The name must be longer than 3 characters
-							</Text>
-						</View>
-						<Pressable
-							onPress={() => SetshowWarning(false)}
-							style={styles.warn_button}
-							android_ripple={{ color: '#ffffff' }}
-						>
-							<Text style={styles.text}>OK</Text>
-						</Pressable>
-					</View>
-				</View>
-			</Modal>
-			<Text style={styles.text}>Please write your name:</Text>
-			<TextInput
-				style={styles.input}
-				placeholder="e.g. Maria"
-				onChangeText={(value) => SetName(value)}
-			/>
-			<TaniButton
-				onPressFunction={onPressHandler}
-				title={submitted ? 'Clear' : 'Submit'}
-				color={'#eb990c'}
-			/>
-			<TaniButton
-				onPressFunction={() => {}}
-				title={'Test'}
-				color={'#4fa729'}
-				style={{ margin: 10 }}
-			/>
-			{/* <Pressable
+		<View style={styles.body}>
+			<Text style={styles.text}>Screen B</Text>
+			<Pressable
 				onPress={onPressHandler}
-				hitSlop={{ top: 10, bottom: 10, right: 10, left: 10 }}
-				android_ripple={{ color: 'yellow' }}
-				style={({ pressed }) => [
-					{ backgroundColor: pressed ? 'pink' : 'green' },
-					styles.button,
-				]}
+				style={({ pressed }) => ({
+					backgroundColor: pressed ? '#ddd' : '#d5a6bd',
+				})}
 			>
-				<Text style={styles.text}>{submitted ? 'Clear' : 'Submit'}</Text>
-			</Pressable> */}
-			{submitted ? (
-				<View style={styles.body}>
-					<Text style={styles.text}>You are registered as {name}</Text>
-					<Image
-						style={styles.image}
-						source={require('../assets/done.png')}
-						resizeMode="stretch"
-					/>
-				</View>
-			) : (
-				<Image
-					style={styles.image}
-					source={require('../assets/error.png')}
-					resizeMode="stretch"
-					blurRadius={5}
-				/>
-			)}
-		</ImageBackground>
+				<Text style={styles.text}>Go back to Screen A</Text>
+			</Pressable>
+		</View>
 	);
-};
+}
+
+function App() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator
+				screenOptions={{
+					header: () => null,
+				}}
+			>
+				<Stack.Screen
+					name="Screen_A"
+					component={ScreenA}
+					// options={{
+					// 	header: () => null,
+					// }}
+				/>
+				<Stack.Screen name="Screen_B" component={ScreenB} />
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
 
 const styles = StyleSheet.create({
 	body: {
 		flex: 1,
+		justifyContent: 'center',
 		alignItems: 'center',
 	},
-
 	text: {
-		color: '#000044',
-		fontSize: 20,
-		margin: 10,
-		textAlign: 'center',
-	},
-	input: {
-		width: 200,
-		borderWidth: 1,
-		borderColor: 'grey',
-		borderRadius: 5,
-		textAlign: 'center',
-		fontSize: 20,
-		margin: 50,
-	},
-	button: {
-		width: 150,
-		height: 50,
-		alignItems: 'center',
-	},
-	centered_view: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#00000099',
-	},
-	warn_modal: {
-		width: 300,
-		height: 300,
-		backgroundColor: '#ffffff',
-		borderWidth: 1,
-		borderColor: '#999999',
-		borderRadius: 20,
-	},
-	warn_title: {
-		height: 50,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#f5f22c',
-		borderTopRightRadius: 20,
-		borderTopLeftRadius: 20,
-	},
-	warn_body: {
-		height: 200,
-		alignItems: 'center',
-		justifyContent: 'center',
-	},
-	warn_button: {
-		backgroundColor: '#4fa729',
-		borderBottomLeftRadius: 20,
-		borderBottomRightRadius: 20,
-	},
-	image: {
-		width: 100,
-		height: 100,
+		fontSize: 40,
+		fontWeight: 'bold',
 		margin: 10,
 	},
 });
